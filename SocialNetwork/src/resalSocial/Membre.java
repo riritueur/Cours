b1cf4d72e297c256cee64ef5c4940c72e2cb830e
Richard Peres <riritueur@gmail.com>
2017-11-15 11:20:38 +0100

    je lait push

---
+++ SocialNetwork/src/resalSocial/Membre.java
@@ -0,0 +1,7 @@
+package resalSocial;
+
+import grapheX.Sommet;
+import reseauSocial.core.MemberInterface;
+
+public class Membre extends Sommet implements MemberInterface{
+}
---
+++ SocialNetwork/src/resalSocial/ReseauSocial.java
@@ -0,0 +1,8 @@
+package resalSocial;
+
+import grapheSimple.GrapheSimple;
+import reseauSocial.core.SocialNetworkInterface;
+
+public class ReseauSocial implements SocialNetworkInterface{
+	GrapheSimple g = new GrapheSimple();
+}
