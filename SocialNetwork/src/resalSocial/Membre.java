d28446934d6fa996f503c47e480dc5a2699cd753
Francois Rigaut <francois.rigaut@etu.unice.fr>
2017-11-15 11:35:30 +0100

    yallaistah

--- SocialNetwork/src/resalSocial/Membre.java
+++ SocialNetwork/src/resalSocial/Membre.java
@@ -1,7 +1,39 @@
 package resalSocial;
 
+import java.util.HashMap;
+
 import grapheX.Sommet;
 import reseauSocial.core.MemberInterface;
 
 public class Membre extends Sommet implements MemberInterface{
+	String nom;
+	int age;
+	String descr;
+	
+	HashMap<Membre, Integer> relations = new HashMap<Membre, Integer>();
+	
+	public Membre(String nom, int age, String descr){
+		super(nom);
+		this.nom = nom;
+		this.age = age;
+		this.descr = descr;
+	}
+
+	@Override
+	public int getAge() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String ident() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- SocialNetwork/src/resalSocial/ReseauSocial.java
+++ SocialNetwork/src/resalSocial/ReseauSocial.java
@@ -1,8 +1,69 @@
 package resalSocial;
 
+import java.util.Collection;
+import java.util.Observable;
+import java.util.Set;
+
+import facebookGhost.FacebookGhostNetwork;
 import grapheSimple.GrapheSimple;
+import reseauSocial.core.MemberInterface;
 import reseauSocial.core.SocialNetworkInterface;
 
 public class ReseauSocial implements SocialNetworkInterface{
 	GrapheSimple g = new GrapheSimple();
+	
+	public ReseauSocial(){}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+	}
+
+	@Override
+	public MemberInterface getMember(String identifier) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<? extends MemberInterface> getMembers() {
+		return (Collection<? extends MemberInterface>) g.sommets();
+	}
+
+	@Override
+	public MemberInterface addMember(String identifier, int age, String description) {
+		Membre m = new Membre(identifier,age,description);
+		g.ajouterSommet(m);
+		return m;
+	}
+
+	@Override
+	public MemberInterface addMember(String ident, boolean belongsToAnotherNetwork) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void relate(int force, MemberInterface member, MemberInterface friend) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<? extends MemberInterface> relateToRank(MemberInterface member, int rank) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int distance(MemberInterface member1, MemberInterface member2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setOtherNetwork(FacebookGhostNetwork f) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
