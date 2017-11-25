da163ecd2ef24f61ab6fd2d19004fa2ed77ec7be
Francois Rigaut <francois.rigaut@etu.unice.fr>
2017-11-25 22:51:05 +0100

    Suppression d'erreur de modélisation dans MembreAutreReseau

--- SocialNetwork/src/resalSocial/MembreAutreReseau.java
+++ SocialNetwork/src/resalSocial/MembreAutreReseau.java
@@ -4,59 +4,9 @@ import java.util.ArrayList;
 
 import facebookGhost.User;
 
-public class MembreAutreReseau extends MembreAbstract implements User{
+public class MembreAutreReseau extends MembreAbstract{
 
 	public MembreAutreReseau(String nom, int age, String descr) {
 		super(nom, age, descr);
-	}
-
-	@Override
-	public String myProfil() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public AgeRange getAgeRange() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ArrayList<User> getFriends() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void addFriend(User friend) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addFamily(User familyMember) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public ArrayList<User> getFamily() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-		
+	}		
 }
