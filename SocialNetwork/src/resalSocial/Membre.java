cee4af097e9a94780cfadfb023f225fe89f258b9
Francois Rigaut <francois.rigaut@etu.unice.fr>
2017-11-28 13:11:28 +0100

    ah

--- SocialNetwork/src/resalSocial/Membre.java
+++ SocialNetwork/src/resalSocial/Membre.java
@@ -5,12 +5,34 @@ import java.util.HashMap;
 import grapheX.Sommet;
 import reseauSocial.core.MemberInterface;
 
-public class Membre extends MembreAbstract implements MemberInterface{
+public class Membre extends Sommet implements MemberInterface{
 	
 	HashMap<Membre, Integer> relations = new HashMap<Membre, Integer>();
 	
+	private String nom;
+	private int age;
+	private String descr;
+
 	public Membre(String nom, int age, String descr){
-		super(nom,age,descr);
+		super(nom);
+		this.nom = nom;
+		this.age = age;
+		this.descr = descr;
+	}
+	
+	@Override
+	public int getAge() {
+		return age;
+	}
+
+	@Override
+	public String getDescription() {
+		return descr;
+	}
+
+	@Override
+	public String ident() {
+		return nom;
 	}
 
 	
